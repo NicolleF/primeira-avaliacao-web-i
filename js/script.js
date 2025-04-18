ab9b2b56cfa60e6521db8a59d93c5cff8a229ba9
NicolleF <nicolleformigari@gmail.com>
2025-04-18 16:03:21 -0300

    Ajustes iniciais para padronização de arquivos + versão inicial do formulário sem estilização

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function createFooter() {
+    return `<footer>
+        <p>Integrantes do projeto: Ana Caroline Henschel, Christian Alves dos Santos e Nicolle Formigari Poncio</p>
+    </footer>`
+}
+
+document.getElementById("footer").innerHTML = createFooter();
